a7445e9896f2d77190282916b13112efcdf59e27
ykkim010201@gmail.com <ykkim010201@gmail.com>
2023-09-30 19:04:35 +0900

    23/09/30 spring 입문편 끝 !

--- hello-spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -34,7 +34,6 @@ public class MemberService {
 //        });
 
         validateDuplicateMember(member);    //중복 회원 검증
-
         memberRepository.save(member);
         return member.getId();
     }
